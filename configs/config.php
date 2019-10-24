b8a3625707b28354e85fe7094df0fb1aa7156b40
CubeWin <47871881+CubeWin@users.noreply.github.com>
2019-10-23 23:51:32 -0500

    Update config.php

--- configs/config.php
+++ configs/config.php
@@ -3,10 +3,10 @@ const LBS = "libraries/";
 const VIEWS = "views/";
 define("DFT", "default/");
 define("RQ", "resources/");
-define("URL", "http://192.168.1.105/sharewin/pdo");
+define("URL", "http://192.168.1.105/sharewin/pdo"); #cambiar
 
-define('HOST', 'localhost');
-define('DB', 'revista');
-define('USER', 'adminExt');
-define('PASSWORD', "Ux1234!!");
+define('HOST', 'localhost'); #cambiar
+define('DB', 'db'); #cambiar
+define('USER', 'root'); #cambiar
+define('PASSWORD', ""); #cambiar
 define('CHARSET', 'utf8mb4');
